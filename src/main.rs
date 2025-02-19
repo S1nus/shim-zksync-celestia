ca6ecb4bb53d481d77311e7f298160f2975c533c
c-node <chuckhead7@gmail.com>
2025-02-19 23:27:03 +0100

    yay almost got algo to get blobstream range proof thingy

--- src/main.rs
+++ src/main.rs
@@ -31,10 +31,19 @@ impl DataCommitmentStored {
     }
 }
 
+async fn get_latest_block(client: &Client<L1>, contract: &Contract) -> U256 {
+    let request = CallRequest {
+        to: Some("0xF0c6429ebAB2e7DC6e05DaFB61128bE21f13cb1e".parse().unwrap()),
+        data: Some(contract.function("latestBlock").unwrap().encode_input(&[]).unwrap().into()),
+        ..Default::default()
+    };
+    let block_num = client.block_number().await.expect("Could not get block number");
+    let result = client.call_contract_function(request, Some(BlockId::Number(block_num.into()))).await.unwrap().0;
+    decode(&[ParamType::Uint(256)], &result).unwrap()[0].clone().into_uint().unwrap()
+}
 
 #[tokio::main]
 async fn main() {
-    
     let contract = Contract::load(File::open("blobstream.json").unwrap()).unwrap();
     let blobstream_update_event = contract.events_by_name("DataCommitmentStored").unwrap()[0].clone();
     println!("event by name: {:?}", blobstream_update_event);
@@ -45,41 +54,67 @@ async fn main() {
     let block_num = client.block_number()
         .await
         .expect("Could not get block number");
-    println!("Block number: {}", block_num);
 
-    let request = CallRequest {
-        to: Some("0xF0c6429ebAB2e7DC6e05DaFB61128bE21f13cb1e".parse().unwrap()),
-        data: Some(contract.function("latestBlock").unwrap().encode_input(&[]).unwrap().into()),
-        ..Default::default()
-    };
-    let result = client.call_contract_function(request, Some(BlockId::Number(block_num.into()))).await.unwrap().0;
-    let latest_block = decode(&[ParamType::Uint(256)], &result).unwrap()[0].clone().into_uint().unwrap();
+    let mut latest_block = get_latest_block(&client, &contract).await;
     println!("Latest blobstream block: {}", latest_block);
-    return;
 
-    println!("Event signature: {:?}", blobstream_update_event.signature());
+    let target_height: u64 = 4774355;
 
-    
-    let filter = FilterBuilder::default()
-        .from_block(BlockNumber::Number(block_num-500))
-        .to_block(BlockNumber::Number(block_num))
-        .address(vec!["0xF0c6429ebAB2e7DC6e05DaFB61128bE21f13cb1e".parse().unwrap()])
-        .topics(
-            Some(vec![blobstream_update_event.signature()]),
-            None,
-            None,
-            None
-        )
-        .build();
-    let logs = client.logs(&filter).await.expect("Could not get logs");
-    let log = logs[0].clone();
-    println!("Logs: {:?}", logs);
-    let data_commitment_stored = DataCommitmentStored::from_log(&log);
-    println!("Data commitment stored: {:?}", data_commitment_stored);
+    let mut from: Option<U256> = None;
+    let mut to: Option<U256> = None;
+    if target_height < latest_block.as_u64() {
+        println!("Target height is less than latest block, searching events...");
+        let mut multiplier = 1;
+        let mut page_start = block_num;
+        let mut page_end = page_start - 500 * multiplier;
+        let mut found = false;
+        while !found {
+            let filter = FilterBuilder::default()
+                .from_block(BlockNumber::Number(page_end))
+                .to_block(BlockNumber::Number(page_start))
+                .address(vec!["0xF0c6429ebAB2e7DC6e05DaFB61128bE21f13cb1e".parse().unwrap()])
+                .topics(
+                    Some(vec![blobstream_update_event.signature()]),
+                    None,
+                    None,
+                    None
+                )
+                .build();
+            let logs = client.logs(&filter).await.expect("Could not get logs");
+            for log in logs {
+                println!("checking a log...");
+                let data_commitment_stored = DataCommitmentStored::from_log(&log);
+                if data_commitment_stored.start_block.as_u64() <= target_height && data_commitment_stored.end_block.as_u64() > target_height {
+                    println!("Found the log!");
+                    found = true;
+                    from = Some(data_commitment_stored.start_block);
+                    to = Some(data_commitment_stored.end_block);
+                    break;
+                }
+            }
+            if !found {
+                println!("No log found, searching in the next page...");
+                multiplier += 1;
+                page_start = page_end;
+                page_end = page_start - 500 * multiplier;
+            }
+        }
+    } else {
+        println!("Target height is greater than latest block, waiting for next update...");
+        from = Some(latest_block);
+        while (latest_block < target_height.into()) {
+            latest_block = get_latest_block(&client, &contract).await;
+            println!("Latest blobstream block: {}", latest_block);
+            tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
+        }
+        to = Some(latest_block);
+    }
+
+    println!("From: {:?}, To: {:?}", from, to);
 
     // TODO: 
     // Get the inclusion proof with these docs https://docs.celestia.org/how-to-guides/blobstream-proof-queries
     let tm_rpc_client = tm_rpc_utils::TendermintRPCClient::new("http://public-celestia-mocha4-consensus.numia.xyz:26657".to_string());
-    let proof = tm_rpc_client.get_data_root_inclusion_proof(data_commitment_stored.start_block.as_u64()+(data_commitment_stored.end_block.as_u64() - data_commitment_stored.start_block.as_u64())/2, data_commitment_stored.start_block.as_u64(), data_commitment_stored.end_block.as_u64() ).await.unwrap();
+    let proof = tm_rpc_client.get_data_root_inclusion_proof(from.unwrap().as_u64()+(to.unwrap().as_u64() - from.unwrap().as_u64())/2, from.unwrap().as_u64(), to.unwrap().as_u64() ).await.unwrap();
     println!("Proof: {:?}", proof);
 }
